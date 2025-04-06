286bbcfe850e710c73d206edfb703ee484cacaff
Zertonixa <olpin9070@gmail.com>
2025-04-06 16:36:01 +0300

    tests

---
+++ tests/test_user.py
@@ -0,0 +1,45 @@
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+# Существующие пользователи
+users = [
+    {
+        'id': 1,
+        'name': 'Ivan Ivanov',
+        'email': 'i.i.ivanov@mail.com',
+    },
+    {
+        'id': 2,
+        'name': 'Petr Petrov',
+        'email': 'p.p.petrov@mail.com',
+    }
+]
+
+def test_get_existed_user():
+    '''Получение существующего пользователя'''
+    response = client.get("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 200
+    assert response.json() == users[0]
+
+def test_get_unexisted_user():
+    '''Получение несуществующего пользователя'''
+    response = client.get("/api/v1/user", params={'email': "wrong_user@gmail.com"})
+    assert response.status_code == 404
+
+def test_create_user_with_valid_email():
+    '''Создание пользователя с уникальной почтой'''
+    response = client.post("/api/v1/user", json={"name": "Alex",'email': "alex.a.a@gmail.com"})
+    assert response.status_code == 201
+
+def test_create_user_with_invalid_email():
+    '''Создание пользователя с почтой, которую использует другой пользователь'''
+    response = client.post("/api/v1/user", json={"name": "Alex",'email': "i.i.ivanov@mail.com"})
+    assert response.status_code == 409
+
+def test_delete_user():
+    '''Удаление пользователя'''
+    response = client.delete("/api/v1/user", params={"email": "i.i.ivanov@mail.com"})
+    assert response.status_code == 204
